6e9fb15a3c51139c34e6df6792e9cdbcace12f91
Kitim Leong <145189370+supercity888@users.noreply.github.com>
2023-10-17 11:01:38 +1300

    initiating first coding

---
+++ Exercise2.py
@@ -0,0 +1,9 @@
+num = input("Enter a number: ")
+enter_num = int(num)
+if enter_num < 0:
+  print("You can't enter a number less than 0")
+  elif enter_num = str:
+  print("Please enter only integers.")
+  num = input("Enter a number: ") 
+else:
+  print("The weight of the object is calculated to be (in Newton):", newton)
